4e6b9bd601369f6b660143d4a705af03d279edd3
NazarPro <nazarpro@gmail.com>
2021-04-22 22:02:47 +0300

    Corrected remarks

--- src/main/java/PropertiesFileReader.java
+++ src/main/java/PropertiesFileReader.java
@@ -1,19 +1,18 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class PropertiesFile {
-    private PropertiesFile() {throw new IllegalStateException("Utility class");}
+public class PropertiesFileReader {
+    private PropertiesFileReader() {throw new IllegalStateException("Utility class");}
 
-    public static String getPropertyValue() throws IOException {
+    public static String getTimeoutPropertyValue() {
         Properties properties = new Properties();
         try(InputStream stream = new FileInputStream("settings.properties"))
             {
                 properties.load(stream);
 
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return properties.getProperty("timeout");
